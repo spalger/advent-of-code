7e8ab7be7c17f99281224c8ceb6ed4472d5e06c2
Spencer Alger <spalger@users.noreply.github.com>
2020-12-07 22:42:51 -0700

    day8 part 2 complete

--- day8/run.js
+++ day8/run.js
@@ -1,5 +1,4 @@
 const Fs = require('fs')
-
 class Instruction {
   constructor(type, arg) {
     this.type = type
@@ -7,7 +6,45 @@ class Instruction {
   }
 }
 
-const instructions = Fs.readFileSync('input.txt', 'utf-8')
+function run(instructions) {
+  const seen = new Set()
+  let acc = 0
+  let i = 0
+
+  while (i < instructions.length) {
+    const inst = instructions[i]
+
+    if (seen.has(inst)) {
+      return {
+        loop: true,
+        acc,
+      }
+    } else {
+      seen.add(inst)
+    }
+
+    switch (inst.type) {
+      case 'acc':
+        acc += inst.arg
+        i++
+        break
+      case 'nop':
+        i++
+        break
+      case 'jmp':
+        i += inst.arg
+        break
+    }
+  }
+
+  return {
+    loop: false,
+    acc,
+  }
+}
+
+/** @type {Instruction[]} */
+const corruptInstructions = Fs.readFileSync('input.txt', 'utf-8')
   .split('\n')
   .filter((l) => l.trim())
   .map((l) => {
@@ -18,30 +55,29 @@ const instructions = Fs.readFileSync('input.txt', 'utf-8')
     )
   })
 
-const seen = new Set()
-let acc = 0
-let i = 0
+const possibleCorruptions = corruptInstructions
+  .map((inst, i) =>
+    inst.type === 'jmp' || inst.type === 'nop' ? i : undefined,
+  )
+  .filter((i) => i !== undefined)
 
-while (true) {
-  const inst = instructions[i]
+for (const possibleCorruptionI of possibleCorruptions) {
+  const inst = corruptInstructions[possibleCorruptionI]
+  const result = run([
+    ...corruptInstructions.slice(0, possibleCorruptionI),
+    new Instruction(inst.type === 'jmp' ? 'nop' : 'jmp', inst.arg),
+    ...corruptInstructions.slice(possibleCorruptionI + 1),
+  ])
 
-  if (seen.has(inst)) {
-    console.log('acc when first instruction was re-executed was', acc)
+  if (!result.loop) {
+    console.log(
+      'replacing',
+      inst,
+      'at',
+      possibleCorruptionI,
+      'allowed the program to run and exit with acc',
+      result.acc,
+    )
     break
-  } else {
-    seen.add(inst)
-  }
-
-  switch (inst.type) {
-    case 'acc':
-      acc += inst.arg
-      i++
-      break
-    case 'nop':
-      i++
-      break
-    case 'jmp':
-      i += inst.arg
-      break
   }
 }
