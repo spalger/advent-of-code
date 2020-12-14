852227bee30ff4f6ea142f38ffb6314e7c432d31
Spencer Alger <spalger@users.noreply.github.com>
2020-12-13 23:42:06 -0700

    day14 part 2 complete

--- 2020/day14/run-program.js
+++ 2020/day14/run-program.js
@@ -19,37 +19,34 @@ const toBin = (dec, mask) => {
   return bin
 }
 
-const fromBin = (bin) => {
-  return bin.split('').reduce((acc, c, i) => {
-    if (c === '0') {
-      return acc
-    }
-
-    return acc + Math.pow(2, binWidth - i)
-  }, 0)
-}
-
 let mask
 const memory = new Map()
+
+const set = (addr, value) => {
+  if (addr.includes('X')) {
+    set(addr.replace('X', '1'), value)
+    set(addr.replace('X', '0'), value)
+  } else {
+    memory.set(addr, value)
+  }
+}
 
 for (const line of input) {
   const [op, value] = line.split(' = ')
   if (op === 'mask') {
     mask = new Map()
     for (const [i, c] of value.split('').entries()) {
-      if (c !== 'X') {
+      if (c !== '0') {
         mask.set(i, c)
       }
     }
   } else {
     const [, addr] = op.split(/[[\]]/)
-    memory.set(addr, toBin(parseInt(value, 10), mask))
+    set(toBin(parseInt(addr, 10), mask), parseInt(value, 10))
   }
 }
 
 console.log(
   'sum of values in memory after exection is',
-  Array.from(memory.values())
-    .map(fromBin)
-    .reduce((acc, n) => acc + n),
+  Array.from(memory.values()).reduce((acc, n) => acc + n),
 )
