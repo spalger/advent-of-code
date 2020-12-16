89753e0eaa1751266b4800d02c912ad69bfdba4d
Spencer Alger <spalger@users.noreply.github.com>
2020-12-16 01:59:45 -0700

    day16 part 1 complete

---
+++ 2020/day16/find-invalid-tickets.js
@@ -0,0 +1,63 @@
+const Fs = require('fs')
+const input = Fs.readFileSync('input.txt', 'utf-8')
+
+class Rule {
+  constructor(name, rangesChunk) {
+    this.name = name
+    this.ranges = rangesChunk.split(' or ').map((r) => {
+      const [from, to] = r.trim().split('-')
+      return { from: parseInt(from, 10), to: parseInt(to, 10) }
+    })
+  }
+
+  match(n) {
+    return this.ranges.some(({ from, to }) => n >= from && n <= to)
+  }
+}
+
+const [rulesChunk, , nearbyTicketsChunk] = input.split('\n\n')
+const rules = rulesChunk
+  .split('\n')
+  .filter((l) => l.trim())
+  .map((l) => {
+    const [name, rangesChunk] = l.split(':')
+    return new Rule(name, rangesChunk)
+  })
+
+class Ticket {
+  constructor(ticketChunk) {
+    this.numbers = ticketChunk
+      .split(',')
+      .map((n) => parseInt(n, 10))
+      .filter((n) => !Number.isNaN(n))
+  }
+}
+
+const nearbyTickets = nearbyTicketsChunk
+  .split('\n')
+  .filter((l) => l.trim())
+  .slice(1)
+  .map((l) => new Ticket(l))
+
+const invalidTickets = new Set()
+const invalidNumbers = []
+for (const ticket of nearbyTickets) {
+  for (const n of ticket.numbers) {
+    if (!rules.some((r) => r.match(n))) {
+      invalidNumbers.push(n)
+      invalidTickets.add(ticket)
+    }
+  }
+}
+
+console.log(
+  invalidTickets.size,
+  'of',
+  nearbyTickets.length,
+  'nearby tickets are invalid',
+)
+
+console.log(
+  'the sum of all invalid numbers is',
+  invalidNumbers.reduce((acc, n) => acc + n),
+)
