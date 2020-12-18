4d262d8db824a1625aec36a25db9970308fae0df
Spencer Alger <spalger@users.noreply.github.com>
2020-12-18 10:28:45 -0700

    remove hack parsing of input

--- 2020/day18/do-math-homework.js
+++ 2020/day18/do-math-homework.js
@@ -1,20 +1,48 @@
 const Fs = require('fs')
 
-/**
- * Hack the input, parse math problems into JSON
- * @param {string} problem
- */
-function parse(problem) {
-  return JSON.parse(
-    `[${problem
-      .replace(/(\+|\*)/g, '"$1"')
-      .split('(')
-      .join('[')
-      .split(')')
-      .join(']')
-      .split(' ')
-      .join(',')}]`,
-  )
+/** @param {string} input */
+function parse(input) {
+  let i = 0
+  const tokens = []
+  for (; i < input.length; i++) {
+    const char = input[i]
+    if (char === ' ') {
+      // ignore spaces
+      continue
+    }
+
+    if (char === '+' || char === '*') {
+      tokens.push(char)
+      continue
+    }
+
+    if (char === '(') {
+      // parse sub-expression
+      const result = parse(input.slice(i + 1))
+      i = i + result.consumed.length
+      tokens.push(result.tokens)
+      continue
+    }
+
+    if (char === ')') {
+      // a closing parenthesis means this sub-expression is complete
+      break
+    }
+
+    // attempt to parse char as number
+    const num = parseInt(char, 10)
+    if (!Number.isNaN(num)) {
+      tokens.push(num)
+      continue
+    }
+
+    throw new Error(`unexpected token "${char}" as position ${i} in "${input}"`)
+  }
+
+  return {
+    consumed: input.slice(0, i + 1),
+    tokens,
+  }
 }
 
 function evaluateAddition(expression) {
@@ -57,7 +85,7 @@ function evaluate(expression) {
 const problems = Fs.readFileSync('input.txt', 'utf-8')
   .split('\n')
   .filter((l) => l.trim())
-  .map((problem) => parse(problem))
+  .map((problem) => parse(problem).tokens)
 
 console.log(
   'the sum of the solutions of each problem is',
