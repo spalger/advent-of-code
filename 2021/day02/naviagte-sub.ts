44e3f23bc65567df3e916c4adb107ac6fbb5f7a7
Spencer <email@spalger.com>
2021-12-02 07:29:32 +0000

    [2021] day 2 complete

--- 2021/day02/naviagte-sub.ts
+++ 2021/day02/naviagte-sub.ts
@@ -40,19 +40,48 @@ function runRoutine(routine: Routine) {
   return { horizontal, depth }
 }
 
+function runRoutineWithAim(routine: Routine) {
+  let horizontal = 0
+  let depth = 0
+  let aim = 0
+
+  for (const { direction, steps } of routine) {
+    switch (direction) {
+      case 'up':
+        aim -= steps
+        break
+      case 'down':
+        aim += steps
+        break
+      case 'forward':
+        horizontal += steps
+        depth += steps * aim
+        break
+    }
+  }
+
+  return { aim, horizontal, depth }
+}
+
 export function test() {
-  const input = dedent`
+  const routine = parseRoutine(dedent`
     forward 5
     down 5
     forward 8
     up 3
     down 8
     forward 2
-  `
+  `)
 
-  deepStrictEqual(runRoutine(parseRoutine(input)), {
+  deepStrictEqual(runRoutine(routine), {
     horizontal: 15,
     depth: 10,
+  })
+
+  deepStrictEqual(runRoutineWithAim(routine), {
+    horizontal: 15,
+    depth: 60,
+    aim: 10,
   })
 }
 
@@ -60,4 +89,10 @@ export function part1(input: string) {
   const pos = runRoutine(parseRoutine(input))
   console.log('final position, horizontal', pos.horizontal, 'depth', pos.depth)
   console.log('point is', pos.horizontal * pos.depth)
+}
+
+export function part2(input: string) {
+  const { depth, horizontal, aim } = runRoutineWithAim(parseRoutine(input))
+  console.log('final position, h', horizontal, 'd', depth, 'a', aim)
+  console.log('point is', horizontal * depth)
 }
