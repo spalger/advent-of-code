9dd643a8ba0046965fb3c19c8b0367a4dce2437a
Spencer <email@spalger.com>
2021-12-04 01:42:36 +0000

    [2021] day 3 part 2 complete

--- 2021/day03/diagnostics.ts
+++ 2021/day03/diagnostics.ts
@@ -9,20 +9,18 @@ function parseBinary(string: string) {
   return num
 }
 
-function findCommons(input: string[]) {
-  const width = input[0].length
-  const result = new Array(width)
-
-  for (let i = 0; i < width; i++) {
-    const count = input.filter((l) => l[i] === '1').length
-    result[i] = count > input.length / 2
-  }
-
-  return result
+function getMostCommon(input: string[], i: number) {
+  const half = input.length / 2
+  const count = input.filter((l) => l[i] === '1').length
+  return count === half ? null : count < half ? 0 : 1
 }
 
 function analyze(input: string[]) {
-  const oneMostCommon = findCommons(input)
+  const width = input[0].length
+  const oneMostCommon = new Array(width)
+  for (let i = 0; i < width; i++) {
+    oneMostCommon[i] = getMostCommon(input, i) === 1
+  }
 
   return {
     gamma: parseBinary(oneMostCommon.map((x) => (x ? 1 : 0)).join('')),
@@ -50,10 +48,64 @@ export function test() {
     gamma: 22,
     episilon: 9,
   })
+
+  const oxygen = findOxygenGeneratorRating(input)
+  console.log('oxy gen:', oxygen)
+
+  const co2 = findCo2ScrubberRating(input)
+  console.log('co2:', co2)
 }
 
 export function part1(input: string) {
   const rates = analyze(toLines(input))
   console.log('rates', rates)
   console.log('power consumption:', rates.episilon * rates.gamma)
+}
+
+function findOxygenGeneratorRating(input: string[]) {
+  const width = input[0].length
+  let options = input.slice(0)
+  for (let i = 0; i < width; i++) {
+    const x = `${getMostCommon(options, i) ?? 1}`
+    options = options.filter((n) => n[i] === x)
+
+    if (options.length === 1) {
+      return parseBinary(options[0])
+    }
+
+    if (options.length === 0) {
+      throw new Error('all numbers excluded')
+    }
+  }
+
+  throw new Error('unable to narrow down input to one number')
+}
+
+function findCo2ScrubberRating(input: string[]) {
+  const width = input[0].length
+  let options = input.slice(0)
+  for (let i = 0; i < width; i++) {
+    const x = `${getMostCommon(options, i) === 0 ? 1 : 0}`
+    options = options.filter((n) => n[i] === x)
+
+    if (options.length === 1) {
+      return parseBinary(options[0])
+    }
+
+    if (options.length === 0) {
+      throw new Error('all numbers excluded')
+    }
+  }
+
+  throw new Error('unable to narrow down input to one number')
+}
+
+export function part2(input: string) {
+  const lines = toLines(input)
+  const oxygen = findOxygenGeneratorRating(lines)
+  const co2 = findCo2ScrubberRating(lines)
+
+  console.log('oxygen:', oxygen)
+  console.log('co2:', co2)
+  console.log('life support rating:', oxygen * co2)
 }
