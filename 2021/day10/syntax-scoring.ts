36f7ae77325aec04fe08c2400e0cb7d66456dc9a
Spencer <email@spalger.com>
2021-12-10 23:28:11 +0000

    [2021] day 10 complete

--- 2021/day10/syntax-scoring.ts
+++ 2021/day10/syntax-scoring.ts
@@ -1,47 +1,87 @@
-import { strictEqual } from 'assert'
+import { deepStrictEqual, strictEqual } from 'assert'
 import { dedent, toLines } from '../../common/string'
 
-type Char = '[' | ']' | '(' | ')' | '{' | '}' | '<' | '>'
+type OpenChar = '[' | '(' | '{' | '<'
+type CloseChar = ']' | ')' | '}' | '>'
+type Char = OpenChar | CloseChar
 
-const scores = {
+const corruptionScores = {
   ')': 3,
   ']': 57,
   '}': 1197,
   '>': 25137,
 }
 
-function scoreCorruption(input: string) {
-  let score = 0
-  parseLine: for (const [lineI, line] of toLines(input).entries()) {
-    const expect: Char[] = []
-    for (const [i, char] of (line.split('') as Char[]).entries()) {
-      switch (char) {
-        case '(':
-          expect.unshift(')')
-          break
-        case '<':
-          expect.unshift('>')
-          break
-        case '[':
-          expect.unshift(']')
-          break
-        case '{':
-          expect.unshift('}')
-          break
-        default:
-          if (char === expect[0]) {
-            expect.shift()
-          } else {
-            console.log('line', lineI, 'corrupted with', char, 'at pos', i)
-            score += scores[char]
-            continue parseLine
+const autocompleteScores = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
+
+function parseLine(
+  line: string,
+): { valid: false; char: CloseChar } | { valid: true; expect: CloseChar[] } {
+  const expect: CloseChar[] = []
+  for (const char of line.split('') as Char[]) {
+    switch (char) {
+      case '(':
+        expect.unshift(')')
+        break
+      case '<':
+        expect.unshift('>')
+        break
+      case '[':
+        expect.unshift(']')
+        break
+      case '{':
+        expect.unshift('}')
+        break
+      default:
+        if (char === expect[0]) {
+          expect.shift()
+        } else {
+          return {
+            valid: false,
+            char,
           }
-      }
+        }
     }
   }
 
-  console.log('corruption score:', score)
-  return score
+  return {
+    valid: true,
+    expect,
+  }
+}
+
+function getMiddleScore(scores: number[]) {
+  return [...scores].sort((a, b) => a - b)[Math.floor(scores.length / 2)]
+}
+
+function score(input: string) {
+  let corruptionScore = 0
+  const lineAutocompleteScores: number[] = []
+
+  for (const line of toLines(input)) {
+    const result = parseLine(line)
+    if (result.valid && result.expect.length) {
+      const score = result.expect.reduce(
+        (acc, c) => acc * 5 + autocompleteScores[c],
+        0,
+      )
+      lineAutocompleteScores.push(score)
+    }
+    if (!result.valid) {
+      corruptionScore += corruptionScores[result.char]
+    }
+  }
+
+  console.log('corruption score:', corruptionScore)
+  const autocompleteScore = getMiddleScore(lineAutocompleteScores)
+  console.log('autocomplete score:', autocompleteScore)
+
+  return { corruptionScore, autocompleteScore }
 }
 
 export function test() {
@@ -58,9 +98,16 @@ export function test() {
     <{([{{}}[<[[[<>{}]]]>[]]
   `
 
-  strictEqual(scoreCorruption(program), 26397)
+  deepStrictEqual(score(program), {
+    corruptionScore: 26397,
+    autocompleteScore: 288957,
+  })
 }
 
 export function part1(input: string) {
-  strictEqual(scoreCorruption(input), 278475)
+  strictEqual(score(input).corruptionScore, 278475)
+}
+
+export function part2(input: string) {
+  strictEqual(score(input).autocompleteScore, 3015539998)
 }
