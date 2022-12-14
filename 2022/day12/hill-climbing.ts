8e638bf8073d74baa158b6fe4b8596d055b5bb2a
spalger <spencer@elastic.co>
2022-12-13 23:28:49 -0600

    [2022] abandon recursive pseudo-code solution for day 12

--- 2022/day12/hill-climbing.ts
+++ 2022/day12/hill-climbing.ts
@@ -8,13 +8,67 @@ function aToN(char: string) {
   return char.charCodeAt(0) - 97
 }
 
-function nToA(number: number) {
-  return String.fromCharCode(number + 97)
+class Solver {
+  constructor(
+    public readonly start: Point,
+    public readonly end: Point,
+    public readonly map: PointMap<number>,
+  ) {}
+
+  private readonly fastestCache = new Map<Point, Map<Point, Point[] | null>>()
+  setFastestPath(from: Point, to: Point, path: Point[] | null) {
+    const tos = this.fastestCache.get(from)
+    if (tos) {
+      tos.set(to, path)
+    } else {
+      this.fastestCache.set(from, new Map([[to, path]]))
+    }
+
+    return path
+  }
+
+  getFastestPath(from: Point, to: Point): Point[] | null {
+    if (from === to) {
+      return []
+    }
+
+    const cached = this.fastestCache.get(from)?.get(to)
+    if (cached !== undefined) {
+      return cached
+    }
+
+    const height = this.map.get(from)
+    let shortest
+    for (const [neighbor, nHeight] of this.map.neighbors(from)) {
+      if (height > nHeight + 1) {
+        continue
+      }
+
+      const next = this.getFastestPath(neighbor, to)
+      if (!shortest || (next && next.length > shortest.length)) {
+        shortest = next
+      }
+    }
+
+    return this.setFastestPath(from, to, shortest ? [from, ...shortest] : null)
+  }
+
+  solve() {
+    // populate cache by walking outward from the end, finding the shortest path from every point to the map
+    const queue = new Set([this.end])
+    for (const point of queue) {
+      this.getFastestPath(point, this.end)
+      for (const [neighbor] of this.map.neighbors(point)) {
+        queue.add(neighbor)
+      }
+    }
+
+    // use the cache to determine the fastest path from the start to the end
+    return this.getFastestPath(this.start, this.end)
+  }
 }
 
-type Maze = { start: Point; end: Point; map: PointMap<number> }
-
-function parse(input: string): Maze {
+function parse(input: string): Solver {
   let start: Point
   let end: Point
   const map = PointMap.fromString(input, (ent, point) => {
@@ -39,106 +93,7 @@ function parse(input: string): Maze {
     throw new Error(`start and/or end points were not defined`)
   }
 
-  return { start, end, map }
-}
-
-type TopoGroup = {
-  id: string
-  points: Point[]
-  next: TopoGroup[]
-}
-
-function topoGroup(maze: Maze) {
-  const groupByPoint = new Map<Point, TopoGroup>()
-  const groups: TopoGroup[] = []
-
-  function getGroup(point: Point) {
-    const existing = groupByPoint.get(point)
-    if (existing) {
-      return existing
-    }
-
-    const nextSteps = new Set<Point>()
-    const ent = maze.map.get(point)
-    const points = new Set([point])
-    for (const p of points) {
-      for (const [n, nent] of maze.map.neighbors(p)) {
-        if (nent === ent) {
-          points.add(n)
-        } else if (nent <= ent + 1) {
-          nextSteps.add(n)
-        }
-      }
-    }
-
-    const group: TopoGroup = {
-      id: `${nToA(ent)} ${
-        Array.from(points).sort((a, b) => {
-          const x = a.x - b.x
-          return x === 0 ? b.y - a.y : x
-        })[0]
-      }`,
-      points: Array.from(points),
-      next: [],
-    }
-
-    // store group in cache without populating "next" so we can
-    // recurse and not duplicate work
-    for (const p of points) {
-      groupByPoint.set(p, group)
-    }
-    groups.push(group)
-
-    for (const n of nextSteps) {
-      const g = getGroup(n)
-      if (!group.next.includes(g)) {
-        group.next.push(g)
-      }
-    }
-
-    return group
-  }
-
-  for (const point of maze.map.points.keys()) {
-    getGroup(point)
-  }
-
-  return {
-    all: groups,
-    byPoint: groupByPoint,
-    get(p: Point) {
-      const g = groupByPoint.get(p)
-      if (!g) {
-        throw new Error(`point ${p} is not in a group`)
-      }
-      return g
-    },
-  }
-}
-
-function solve(maze: Maze) {
-  const groups = topoGroup(maze)
-  const s = groups.get(maze.start)
-  const e = groups.get(maze.end)
-
-  const valid: TopoGroup[][] = []
-  const queue: TopoGroup[][] = [[s]]
-  while (queue.length) {
-    const path = shift(queue)
-    for (const next of path[0].next) {
-      if (path.includes(next)) {
-        continue
-      } else {
-        if (next === e) {
-          valid.push([e, ...path].reverse())
-        } else {
-          queue.push([next, ...path])
-        }
-      }
-    }
-  }
-
-  console.log(valid)
+  return new Solver(start, end, map)
 }
 
 export function test() {
@@ -152,14 +107,14 @@ export function test() {
     `,
   )
 
-  deepStrictEqual(solve(maze), 31)
+  deepStrictEqual(maze.solve(), 31)
 }
 
 export function part1(input: string) {
   const maze = parse(input)
   console.log(
     'the shortest path to the signal strength is',
-    solve(maze),
+    maze.solve(),
     'steps',
   )
 }
