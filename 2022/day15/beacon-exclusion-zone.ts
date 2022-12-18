3496ed899fc316e6d8e6342a4b1eb38b5928e728
spalger <spencer@elastic.co>
2022-12-17 23:56:38 -0600

    [2022] day 15 complete

--- 2022/day15/beacon-exclusion-zone.ts
+++ 2022/day15/beacon-exclusion-zone.ts
@@ -1,7 +1,8 @@
-import { deepStrictEqual } from 'assert'
+import { strictEqual } from 'assert'
 import { toLines } from '../../common/string'
 import { toInt } from '../../common/number'
 import { p, Point } from '../../common/point'
+import { last } from '../../common/array'
 
 const SENSOR_READING_RE =
   /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
@@ -27,10 +28,37 @@ function parse(input: string) {
   })
 }
 
-type XRange = [number, number]
-function getKnownEmptySpace(readings: Reading[], yOfInterest: number) {
-  const ranges = readings
-    .flatMap((r): XRange[] => {
+// reduce range set to the minimal set of ranges
+function reduceCoverage(ranges: XRange[]) {
+  return (
+    [...ranges]
+      // sort ranges by starting x position asc, if those are the same the ending x postition desc
+      .sort((a, b) => a[0] - b[0] || b[1] - a[1])
+      .reduce((acc: XRange[], range): XRange[] => {
+        if (!acc.length) {
+          return [range]
+        }
+
+        const prev = last(acc)
+        // if the start of this range is beyond the end of the previous one, keep it
+        if (range[0] > prev[1]) {
+          return [...acc, range]
+        }
+
+        // drop this range if it is completely encompassed by previous range
+        if (range[1] <= prev[1]) {
+          return acc
+        }
+
+        // extend the previous range if this goes beyond it
+        return [...acc.slice(0, -1), [prev[0], range[1]]]
+      }, [])
+  )
+}
+
+function getCoverage(readings: Reading[], yOfInterest: number) {
+  return reduceCoverage(
+    readings.flatMap((r): XRange[] => {
       const radius = r.center.mdist(r.beacon)
       const dist = radius - Math.abs(yOfInterest - r.center.y)
       if (dist < 0) {
@@ -39,33 +67,17 @@ function getKnownEmptySpace(readings: Reading[], yOfInterest: number) {
 
       // return a range covering the point included in this, inclusive on the left and exclusive on the right
       return [[r.center.x - dist, r.center.x + dist + 1]]
-    })
-    // sort ranges by starting x position asc, if those are the same the ending x postition desc
-    .sort((a, b) => a[0] - b[0] || b[1] - a[1])
+    }),
+  )
+}
 
-  // reduce range set to the minimal set of ranges
-  const minRanges = ranges.reduce((acc: XRange[], range): XRange[] => {
-    if (!acc.length) {
-      return [range]
-    }
-
-    const prev = acc[0]
-    // if the start of this range is beyond the end of the previous one, keep it
-    if (range[0] > prev[1]) {
-      return [...acc, range]
-    }
-
-    // drop this range if it is completely encompassed by previous range
-    if (range[1] <= prev[1]) {
-      return acc
-    }
-
-    // extend the previous range if this goes beyond it
-    return [...acc.slice(0, -1), [prev[0], range[1]]]
-  }, [])
-
+type XRange = [number, number]
+function getKnownEmptySpace(readings: Reading[], yOfInterest: number) {
   // sum the absolute distance between the start and end of each range
-  const sum = minRanges.reduce((acc, r) => acc + Math.abs(r[0] - r[1]), 0)
+  const sum = getCoverage(readings, yOfInterest).reduce(
+    (acc, r) => acc + Math.abs(r[0] - r[1]),
+    0,
+  )
 
   // find the list of beacons in this row because we know there are becons there
   const beacons = new Set(
@@ -75,9 +87,31 @@ function getKnownEmptySpace(readings: Reading[], yOfInterest: number) {
   return sum - beacons.size
 }
 
+function findUncoveredSpace(readings: Reading[], maxCoord: number) {
+  for (let y = 0; y <= maxCoord; y++) {
+    const ranges = getCoverage(readings, y)
+
+    const filteredRanges = ranges.flatMap((r): XRange[] => {
+      // any range which ends before 0 or starts after maxCoord is completely out of bounds
+      if (r[1] < 0 || r[0] > maxCoord) {
+        return []
+      }
+
+      return [[Math.max(0, r[0]), Math.min(maxCoord, r[1])]]
+    })
+
+    if (filteredRanges.length === 2) {
+      return p(filteredRanges[0][1], y)
+    }
+  }
+
+  throw new Error('unable to find uncovered space')
+}
+
 export function test(input: string) {
   const readings = parse(input)
-  deepStrictEqual(getKnownEmptySpace(readings, 10), 26)
+  strictEqual(getKnownEmptySpace(readings, 10), 26)
+  strictEqual(findUncoveredSpace(readings, 20), p(14, 11))
 }
 
 export function part1(input: string) {
@@ -86,5 +120,16 @@ export function part1(input: string) {
     'based on the readings there are',
     getKnownEmptySpace(readings, 2_000_000),
     'points where beacons can not be',
+  )
+}
+
+export function part2(input: string) {
+  const readings = parse(input)
+  const pos = findUncoveredSpace(readings, 4_000_000)
+  console.log(
+    'the only valid location for a beacon based on the readings is',
+    pos,
+    'and its tuning freq is',
+    pos.x * 4_000_000 + pos.y,
   )
 }
